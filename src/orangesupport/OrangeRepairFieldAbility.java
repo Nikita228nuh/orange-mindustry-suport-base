9e211b0ffd373eff0371245b083f1d8a3d9189e8
Nikita228nuh <nikitakobzov08@gmail.com>
2026-09-21 17:43:09 +0200

    Implement OrangeRepairFieldAbility for healing units

---
+++ src/orangesupport/OrangeRepairFieldAbility.java
@@ -0,0 +1,49 @@
+package orangesupport;
+
+import arc.util.Time;
+import mindustry.entities.abilities.Ability;
+import mindustry.gen.Building;
+import mindustry.gen.Unit;
+
+import static mindustry.Vars.indexer;
+
+public class OrangeRepairFieldAbility extends Ability {
+
+    private final float healAmount;
+    private final float range;
+
+    public OrangeRepairFieldAbility(float healAmount, float range) {
+        this.healAmount = healAmount;
+        this.range = range;
+    }
+
+    @Override
+    public void update(Unit unit) {
+        float heal = healAmount * Time.delta / 60f;
+
+        // Repair friendly units.
+        mindustry.entities.Units.nearby(
+            unit.team,
+            unit.x,
+            unit.y,
+            range,
+            other -> {
+                if (other != unit && other.damaged()) {
+                    other.heal(heal);
+                }
+            }
+        );
+
+        // Repair friendly buildings.
+        indexer.allBuildings(
+            unit.x,
+            unit.y,
+            range,
+            building -> {
+                if (building.team == unit.team && building.damaged()) {
+                    building.heal(heal);
+                }
+            }
+        );
+    }
+}
