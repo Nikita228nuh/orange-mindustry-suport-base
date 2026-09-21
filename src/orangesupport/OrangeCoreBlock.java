500ad1d2eb333f161ec574992b3cff842de6eafc
Nikita228nuh <nikitakobzov08@gmail.com>
2026-09-21 17:49:40 +0200

    Add OrangeCoreBlock with health regeneration feature

---
+++ src/orangesupport/OrangeCoreBlock.java
@@ -0,0 +1,34 @@
+package orangesupport;
+
+import arc.util.Time;
+import mindustry.world.blocks.storage.CoreBlock;
+
+public class OrangeCoreBlock extends CoreBlock {
+
+    public OrangeCoreBlock(String name) {
+        super(name);
+
+        update = true;
+        health = 1000;
+        itemCapacity = 1000000;
+        unitCapModifier = 50;
+    }
+
+    @Override
+    public Building createBuilding() {
+        return new OrangeCoreBuild();
+    }
+
+    public class OrangeCoreBuild extends CoreBuild {
+
+        @Override
+        public void updateTile() {
+            super.updateTile();
+
+            // Regenerate 10 HP per second.
+            if (health < maxHealth) {
+                heal(10f * Time.delta / 60f);
+            }
+        }
+    }
+}
