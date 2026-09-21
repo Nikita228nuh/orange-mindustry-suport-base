4128a723ccc5ecb521acf8f0ee5dffe30609a249
Nikita228nuh <nikitakobzov08@gmail.com>
2026-09-21 19:26:50 +0200

    Update OrangeSupportMod.java

--- src/main/java/orangesupport/OrangeSupportMod.java
+++ src/main/java/orangesupport/OrangeSupportMod.java
@@ -1,66 +1,90 @@
 package orangesupport;
 
 import arc.graphics.Color;
+import mindustry.content.Blocks;
+import mindustry.content.TechTree;
 import mindustry.entities.abilities.ForceFieldAbility;
 import mindustry.gen.UnitEntity;
 import mindustry.mod.Mod;
+import mindustry.type.ItemStack;
 import mindustry.type.UnitType;
+import mindustry.world.Block;
+import mindustry.world.meta.BuildVisibility;
 
 public class OrangeSupportMod extends Mod {
 
     public static UnitType orangeSupport;
+    public static Block orangeCore;
 
     @Override
     public void loadContent() {
 
+        // Orange player-controlled support unit.
         orangeSupport = new UnitType("orange-support") {{
             constructor = UnitEntity::create;
 
-            // Movement
             speed = 60f / 8f;
-
-            // Health
             health = 100000f;
+            hitSize = 10f;
 
-            // Player controlled
             playerControllable = true;
             logicControllable = true;
 
-            // Cannot attack
             canAttack = false;
             targetAir = false;
             targetGround = false;
-            weapons.clear();
 
-            // Fast building
             buildSpeed = 1000f;
             buildRange = 80f;
 
-            // Fast mining
             mineSpeed = 1000f;
             mineTier = 999;
             mineRange = 80f;
             mineWalls = true;
             mineFloor = true;
 
-            // Large inventory
             itemCapacity = 1000000;
 
-            // Orange
+            // FREE research.
+            researchCostMultiplier = 0f;
+
             outlineColor = Color.valueOf("ff7a00");
 
-            // 10-block shield
             abilities.add(new ForceFieldAbility(
                 80f,
                 1666.6667f,
                 1000000f,
-                0f
+                360f
             ));
 
-            // Repair nearby units and buildings
-            abilities.add(
-                new OrangeRepairFieldAbility(100f, 80f)
-            );
+            abilities.add(new OrangeRepairFieldAbility(100f, 80f));
         }};
+
+        // Orange Core.
+        orangeCore = new OrangeCoreBlock("orange-core") {{
+            health = 1000;
+            itemCapacity = 1000000;
+            unitCapModifier = 50;
+
+            // FREE to place.
+            requirements = ItemStack.empty;
+
+            buildVisibility = BuildVisibility.shown;
+            alwaysUnlocked = true;
+
+            size = 3;
+        }};
+    }
+
+    @Override
+    public void init() {
+        super.init();
+
+        // Put both items into the Serpulo tech tree.
+        TechTree.node(Blocks.coreShard, () -> {
+            TechTree.node(orangeCore, ItemStack.empty, () -> {
+                TechTree.node(orangeSupport, ItemStack.empty, () -> {});
+            });
+        });
     }
 }
