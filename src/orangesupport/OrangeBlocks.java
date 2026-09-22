805937baec03f4abfe829cb8484291f975990a23
Nikita228nuh <nikitakobzov08@gmail.com>
2026-09-22 20:10:47 +0200

    Update OrangeBlocks.java

--- src/orangesupport/OrangeBlocks.java
+++ src/orangesupport/OrangeBlocks.java
@@ -1,6 +1,6 @@
 package orangesupport;
 
-import mindustry.world.Block;
+import mindustry.content.Blocks;
 import mindustry.world.blocks.storage.CoreBlock;
 
 public class OrangeBlocks {
@@ -9,5 +9,8 @@ public class OrangeBlocks {
 
     public static void load() {
         orangeCore = new OrangeCoreBlock("orange-core");
+
+        // Put the Orange Core in the same category as vanilla cores.
+        orangeCore.category = Blocks.coreShard.category;
     }
 }
