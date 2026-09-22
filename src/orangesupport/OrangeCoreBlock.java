92800e3c454403ff61533163117445dc33720429
Nikita228nuh <nikitakobzov08@gmail.com>
2026-09-22 19:15:01 +0200

    Update OrangeCoreBlock.java

--- src/orangesupport/OrangeCoreBlock.java
+++ src/orangesupport/OrangeCoreBlock.java
@@ -7,14 +7,12 @@ public class OrangeCoreBlock extends CoreBlock {
     public OrangeCoreBlock(String name) {
         super(name);
 
-        // Core properties
         size = 3;
         health = 1000;
         itemCapacity = 1000000;
         unitCapModifier = 50;
 
-        // Make it available for building.
+        // Free to place for testing.
         requirements = new mindustry.type.ItemStack[0];
-        buildVisibility = BuildVisibility.shown;
     }
 }
