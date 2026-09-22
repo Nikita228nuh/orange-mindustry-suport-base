8d0559efdaf1f3013ab167185baac89c80109808
Nikita228nuh <nikitakobzov08@gmail.com>
2026-09-22 19:20:16 +0200

    Update OrangeSupportMod.java

--- src/orangesupport/OrangeSupportMod.java
+++ src/orangesupport/OrangeSupportMod.java
@@ -4,11 +4,6 @@ import mindustry.mod.Mod;
 
 public class OrangeSupportMod extends Mod {
 
-    public static OrangeCoreBlock orangeCore;
-
-    public OrangeSupportMod() {
-    }
-
     @Override
     public void loadContent() {
         OrangeBlocks.load();
