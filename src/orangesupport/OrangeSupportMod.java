08844120b9cd3bf0a18fbbf63341a0697195c400
Nikita228nuh <nikitakobzov08@gmail.com>
2026-09-22 16:17:35 +0200

    Update OrangeSupportMod.java

--- src/orangesupport/OrangeSupportMod.java
+++ src/orangesupport/OrangeSupportMod.java
@@ -1,25 +1,15 @@
 package orangesupport;
 
 import mindustry.mod.Mod;
-import mindustry.type.Category;
-import mindustry.world.Block;
-import mindustry.world.meta.BuildVisibility;
 
 public class OrangeSupportMod extends Mod {
 
-    public static Block orangeTest;
+    public OrangeSupportMod() {
+        // Empty constructor
+    }
 
     @Override
     public void loadContent() {
-
-        orangeTest = new Block("orange-test") {{
-            size = 3;
-            health = 1000;
-
-            category = Category.effect;
-            buildVisibility = BuildVisibility.shown;
-
-            requirements = new mindustry.type.ItemStack[0];
-        }};
+        // Empty for testing
     }
 }
