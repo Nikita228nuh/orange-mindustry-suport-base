f79fa8f169d299b7181a6053e54194ec78d7d5d0
Nikita228nuh <nikitakobzov08@gmail.com>
2026-09-22 06:10:03 +0200

    Update OrangeSupportMod.java

--- src/main/java/orangesupport/OrangeSupportMod.java
+++ src/main/java/orangesupport/OrangeSupportMod.java
@@ -8,8 +8,8 @@ import mindustry.mod.Mod;
 import mindustry.type.ItemStack;
 import mindustry.type.UnitType;
 import mindustry.world.Block;
-import mindustry.world.blocks.storage.CoreBlock;
 import mindustry.world.meta.BuildVisibility;
+import mindustry.world.meta.Category;
 
 public class OrangeSupportMod extends Mod {
 
@@ -26,46 +26,33 @@ public class OrangeSupportMod extends Mod {
         orangeSupport = new UnitType("orange-support") {{
             constructor = UnitEntity::create;
 
-            // 100,000 HP
             health = 100000f;
-
-            // Very fast movement
             speed = 7.5f;
-
-            // 10-block size
             hitSize = 10f;
 
-            // Player controlled
             playerControllable = true;
             logicControllable = true;
 
-            // No attacking
             canAttack = false;
             targetAir = false;
             targetGround = false;
-            weapons.clear();
 
-            // Very fast building
             buildSpeed = 1000f;
             buildRange = 80f;
 
-            // Very fast mining
             mineSpeed = 1000f;
             mineTier = 999;
             mineRange = 80f;
             mineWalls = true;
             mineFloor = true;
 
-            // 1,000,000 inventory
             itemCapacity = 1000000;
 
-            // Orange
-            outlineColor = Color.valueOf("ff7a00");
-
-            // Free research
             researchCostMultiplier = 0f;
 
-            // 1,000,000 shield
+            outlineColor = Color.valueOf("ff7a00");
+
+            // Large shield
             abilities.add(new ForceFieldAbility(
                 80f,
                 1666.6667f,
@@ -73,12 +60,11 @@ public class OrangeSupportMod extends Mod {
                 360f
             ));
 
-            // Repair field
+            // Repair nearby units/buildings
             abilities.add(
                 new OrangeRepairFieldAbility(100f, 80f)
             );
 
-            // Always unlocked
             alwaysUnlocked = true;
         }};
 
@@ -88,35 +74,31 @@ public class OrangeSupportMod extends Mod {
         // =========================
 
         orangeCore = new OrangeCoreBlock("orange-core") {{
-
-            // 3 x 3
             size = 3;
 
-            // 1000 HP
             health = 1000;
-
-            // 1,000,000 storage
             itemCapacity = 1000000;
-
-            // +50 unit capacity
             unitCapModifier = 50;
 
-            // FREE
+            // Free to build
             requirements = ItemStack.empty;
 
-            // Show in build menu
+            // SHOW IN BUILD MENU
             buildVisibility = BuildVisibility.shown;
 
-            // Always unlocked
+            // Put it in the Blocks > Units category
+            category = Category.units;
+
+            // No research needed
             alwaysUnlocked = true;
 
-            // The Orange Support unit comes from this core
+            // This core produces the Orange Support unit
             unitType = orangeSupport;
         }};
 
 
         // =========================
-        // RESEARCH TREE
+        // TECH TREE
         // =========================
 
         TechTree.node(orangeCore, ItemStack.empty, () -> {
