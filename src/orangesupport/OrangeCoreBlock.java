1ad3f129837b380b65f40e5eb9a282e86a2f992c
Nikita228nuh <nikitakobzov08@gmail.com>
2026-09-22 19:44:43 +0200

    Refactor OrangeCoreBlock health and unit type settings

--- src/orangesupport/OrangeCoreBlock.java
+++ src/orangesupport/OrangeCoreBlock.java
@@ -1,8 +1,6 @@
 package orangesupport;
 
-import mindustry.game.Team;
 import mindustry.type.ItemStack;
-import mindustry.world.Tile;
 import mindustry.world.blocks.storage.CoreBlock;
 
 public class OrangeCoreBlock extends CoreBlock {
@@ -11,22 +9,13 @@ public class OrangeCoreBlock extends CoreBlock {
         super(name);
 
         size = 3;
-        health = 5000f;
+        health = 5000;
 
-        itemCapacity = 1000000;
+        // No custom healing here.
+        // The normal Mindustry CoreBlock handles core behavior.
 
-        unitCapModifier = 50;
-
-        // No resources required to place it.
-        requirements = ItemStack.empty;
-
-        // Use our own support unit instead of Alpha.
         unitType = OrangeUnits.orangeSupport;
-    }
 
-    @Override
-    public boolean canPlaceOn(Tile tile, Team team, int rotation) {
-        // Allow the Orange Core to be placed on normal buildable terrain.
-        return tile != null && tile.block().isAir();
+        requirements = ItemStack.empty;
     }
 }
