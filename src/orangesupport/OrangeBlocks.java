299a472835c0ac044bb22e828d0fd4b97cd39514
Nikita228nuh <nikitakobzov08@gmail.com>
2026-09-22 19:11:54 +0200

    Update OrangeBlocks.java

--- src/orangesupport/OrangeBlocks.java
+++ src/orangesupport/OrangeBlocks.java
@@ -1,21 +1,10 @@
 package orangesupport;
 
-import mindustry.type.Category;
-
 public class OrangeBlocks {
 
     public static OrangeCoreBlock orangeCore;
 
     public static void load() {
         orangeCore = new OrangeCoreBlock("orange-core");
-
-        // Put the core in the build menu.
-        orangeCore.category = Category.effect;
-
-        // No items required to build it.
-        orangeCore.requirements = new mindustry.type.ItemStack[0];
-
-        // Make it visible without research.
-        orangeCore.alwaysUnlocked = true;
     }
 }
