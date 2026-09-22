74d54ab8f8d9b76d0c3761496add3822f8316102
Nikita228nuh <nikitakobzov08@gmail.com>
2026-09-22 18:28:32 +0200

    Refactor OrangeSupportMod constructor and load method

--- src/orangesupport/OrangeSupportMod.java
+++ src/orangesupport/OrangeSupportMod.java
@@ -4,10 +4,7 @@ import mindustry.mod.Mod;
 
 public class OrangeSupportMod extends Mod {
 
-    public static OrangeCoreBlock orangeCore;
-
-    @Override
-    public void loadContent() {
-        orangeCore = new OrangeCoreBlock("orange-core");
+    public OrangeSupportMod() {
+        new OrangeBlocks().load();
     }
 }
