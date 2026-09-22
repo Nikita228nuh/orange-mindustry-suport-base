bcbb5cd41400ce60a0df615f29d756b9bd24ab1d
Nikita228nuh <nikitakobzov08@gmail.com>
2026-09-22 19:37:53 +0200

    Update OrangeBlocks.java

--- src/orangesupport/OrangeBlocks.java
+++ src/orangesupport/OrangeBlocks.java
@@ -1,10 +1,16 @@
 package orangesupport;
 
+import mindustry.world.meta.BuildVisibility;
+import mindustry.world.meta.Category;
+
 public class OrangeBlocks {
 
     public static OrangeCoreBlock orangeCore;
 
     public static void load() {
         orangeCore = new OrangeCoreBlock("orange-core");
+
+        orangeCore.category = Category.effect;
+        orangeCore.buildVisibility = BuildVisibility.shown;
     }
 }
