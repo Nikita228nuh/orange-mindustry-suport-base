334121f82c07cb4006e7a8203bea786c2702c3ad
Nikita228nuh <nikitakobzov08@gmail.com>
2026-09-22 20:06:40 +0200

    Update OrangeBlocks.java

--- src/orangesupport/OrangeBlocks.java
+++ src/orangesupport/OrangeBlocks.java
@@ -8,5 +8,13 @@ public class OrangeBlocks {
 
     public static void load() {
         orangeCore = new OrangeCoreBlock("orange-core");
+
+        // Make it usable even without normal build requirements.
+        orangeCore.requirements = new mindustry.type.ItemStack[0];
+        orangeCore.buildTime = 1f;
+        orangeCore.buildCostMultiplier = 0f;
+
+        // Allow it to function on normal terrestrial planets.
+        orangeCore.envEnabled = mindustry.world.meta.Env.terrestrial;
     }
 }
