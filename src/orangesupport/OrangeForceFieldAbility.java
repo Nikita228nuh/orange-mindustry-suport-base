70fdab61baf8021e8a455e6aeefcd88a83bd485d
Nikita228nuh <nikitakobzov08@gmail.com>
2026-09-23 15:39:17 +0200

    Create OrangeForceFieldAbility.java

---
+++ src/orangesupport/OrangeForceFieldAbility.java
@@ -0,0 +1,15 @@
+package orangesupport;
+
+import mindustry.entities.abilities.ForceFieldAbility;
+
+public class OrangeForceFieldAbility extends ForceFieldAbility {
+
+    public OrangeForceFieldAbility(
+        float radius,
+        float regen,
+        float max,
+        float cooldown
+    ) {
+        super(radius, regen, max, cooldown);
+    }
+}
